1f2bd89acd5a1f1fbd995966d1cf2d487252def8
mwangianthony991 <56022200+mwangianthony991@users.noreply.github.com>
2020-02-02 13:20:28 +0300

    Update email.php

--- email.php
+++ email.php
@@ -40,8 +40,8 @@ try {
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'weruproject7';                 // SMTP username
-    $mail->Password = 'Weru2072';                           // SMTP password
+    $mail->Username = '*********';                 // SMTP username
+    $mail->Password = '*********';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
